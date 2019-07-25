4c1efdacec826f62fba76eb01cde6c438d14d45a
wyoliveira <34794785+wyoliveira@users.noreply.github.com>
2019-07-25 11:16:24 -0300

    Método para alterar cor de objeto
    Incluído o método showCurrentColorInRectangle que pega uma String, transforma em decimal, converte esse decimal em um hexadecimal e cria uma cor com esse hexadecimal. Em seguida, pinta um objeto do tipo Node, podendo ser um retangulo, um texto, um botão...

--- ConvertStringColor.kt
+++ ConvertStringColor.kt
@@ -1,6 +1,7 @@
 package com.wyboltech.views
 
 
+
 fun showCurrentColorInRectangle(numberColor: String): String{
 	val text: String = numberColor
 	return Integer.toHexString(text.toInt())
@@ -10,11 +11,26 @@ fun showCurrentColorInRectangle(numberColor: String): String{
 
 
 fun main(args: Array<String>) {
-	//"7925089"
-	//"43775"
+	//"7925089" Verde claro
+	//"43775" Azul claro
+	//"187" Azul escuro
 	
 	val stringToFormat = showCurrentColorInRectangle("11861912")
 	val formatZeroPad: String  = "000000";
 	val formattedHexColor = formatZeroPad.substring(stringToFormat.length) + stringToFormat
 	println(formattedHexColor)
+}
+
+
+@FXML
+fun showCurrentColorInRectangle(numberColor: String){
+	val formatColor = Integer.toHexString(numberColor.toInt())
+	//Define o padrão de 6 dígitos, caso a string de retorno tenha menos que 6 casas, preenche as casas à esquerda com zero
+	val formatZeroPad: String  = "000000" 	
+	//Converte o inteiro em hexadecimal
+	val formattedHexColor = formatZeroPad.substring(formatColor.length) + formatColor
+	//Usa o hexadecimal para criar uma Cor
+	val color : Color = Color.web(formattedHexColor.toUpperCase())
+	//Pinta um retângulo
+	<Shape: Node>.fill = color
 }
